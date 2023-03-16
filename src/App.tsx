2664e5c05c6efec684a9111c49d34862fb532837
jhlee <wisdom_13_@naver.com>
2023-03-16 18:56:34 +0900

    react-hook-form 사용하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useForm, Controller } from 'react-hook-form'
+import { Button, Input } from '@mui/material';
+
+interface Form {
+  age: number
+}
 
 function App() {
+  const { handleSubmit, control, formState: { errors } } = useForm();
+  const [result, setResult] = useState('');
+
+  const onSubmit = (data: Form) => {
+    setResult(JSON.stringify(data));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit as any)}>
+      <div className='container'>
+        <Controller
+          name="age"
+          control={control}
+          defaultValue={33}
+          rules={{ required: true, min: 17, max: 40 }}
+          render={({ field }) => <Input {...field} />}
+        />
+        {
+          errors.age?.type === 'required' && <span>Age is required</span>
+        }
+        {
+          errors.age?.type === 'min' && <span>Mininum Age is 17</span>
+        }
+        {
+          errors.age?.type === 'max' && <span>Maximum Age is 40</span>
+        }
+      </div>
+      <br />
+      <Button type="submit" variant="outlined">Submit</Button>
+      <p>{result}</p>
+    </form>
   );
 }
 
