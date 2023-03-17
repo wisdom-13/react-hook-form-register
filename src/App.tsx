8d15bca46e27f4af33bfb1df7224eb2b9ecef283
jhlee <wisdom_13_@naver.com>
2023-03-17 21:49:07 +0900

    Register fields - 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form'
 
-interface Form {
-  name: string;
-  password: string;
-}
 
 function App() {
-  const { register, handleSubmit } = useForm();
-  const [result, setResult] = useState('');
+  const { register, watch } = useForm();
 
-  const onSubmit = (data: Form) => setResult(JSON.stringify(data));
+  const watchNickname = watch('nickname', 'Easton');
 
   return (
-    <form onSubmit={handleSubmit(onSubmit as any)}>
-      <input type="text" {...register('name')} placeholder="User Name" />
-      <input type="password" {...register('password')} placeholder="Password" />
-      <input type="submit" />
-      <p>{result}</p>
+    <form>
+      <input type="text" placeholder='Nick Name' {...register('nickname')} />
+      <p>{watchNickname}</p>
     </form>
   );
 }
